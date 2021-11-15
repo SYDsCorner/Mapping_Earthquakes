4e51ae61bbff51c7eb2e7439d311679d6fa6725b
Siyada <Siyada_World@outlook.com>
2021-11-15 00:34:59 -0500

    Map Multiple Points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,14 +2,14 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-// let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+// let map = L.map('mapid').setView([40.70, -94.5], 4);
 
 // Create the map object with a center and zoom level.
 // This method is useful when we need to add multiple tile layers, 
 // or a background image of our map(s)
 let map = L.map("mapid", {
     center: [
-        34.0522, -118.2437
+        40.70, -94.5
     ],
     zoom: 4
 });
